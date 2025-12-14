e25e0bc5d4dd0f25d73cb6ec5704ceb59a95758b
BarbossHack <barbossh4ck@protonmail.com>
2025-12-14 23:01:50 +0100

    Add symbol prefixing feature for BoringSSL (#103)
    * Add symbol prefixing feature for BoringSSL
    
    * Fix clippy issues (default and bindgen)
    
    * Temporary patch to fix windows CI
    
    * Drop `i686-linux-android` support in `prefix-symbols` CI

--- boring-sys/src/lib.rs
+++ boring-sys/src/lib.rs
@@ -21,7 +21,8 @@ use std::os::raw::{c_char, c_int, c_uint, c_ulong};
     clippy::derive_partial_eq_without_eq,
     clippy::ptr_offset_with_cast,
     unpredictable_function_pointer_comparisons, // TODO: remove Eq/PartialEq in v5
-    dead_code
+    dead_code,
+    unnecessary_transmutes
 )]
 mod generated {
     include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
